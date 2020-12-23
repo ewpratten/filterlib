af374d00f12d4a6468f12e140a9b2278a9d7d516
Evan Pratten <ewpratten@gmail.com>
2020-12-23 13:32:59 -0500

    Finish up logic

--- src/main/java/ca/retrylife/filterlib/Filter.java
+++ src/main/java/ca/retrylife/filterlib/Filter.java
@@ -1,7 +1,13 @@
 package ca.retrylife.filterlib;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Set;
+import java.util.Map.Entry;
 import java.util.function.BiConsumer;
 import java.util.function.BiFunction;
 import java.util.function.Consumer;
@@ -9,24 +15,36 @@ import java.util.function.Function;
 
 import javax.annotation.Nullable;
 
+import com.google.common.collect.ImmutableList;
+
 public class Filter<T> {
 
-    private HashMap<T, @Nullable Double> scores = new HashMap<>();
+    // Mapping of all objects to their scores
+    private HashMap<T, @Nullable Double> objectScores = new HashMap<>();
+
+    // Ordered list of objects by their score
+    private List<T> orderedObjects = null;
 
     public Filter(List<T> items) {
 
         // Handle mapping all items
         items.forEach((T item) -> {
-            this.scores.put(item, 0.0);
+            this.objectScores.put(item, 0.0);
         });
+
+        // Reset the ordered list cache
+        this.orderedObjects = null;
     }
 
     public Filter(T[] items) {
 
         // Handle mapping all items
         for (T item : items) {
-            this.scores.put(item, 0.0);
+            this.objectScores.put(item, 0.0);
         }
+
+        // Reset the ordered list cache
+        this.orderedObjects = null;
     }
 
     public void score(Function<T, Double> fun) {
@@ -42,8 +60,11 @@ public class Filter<T> {
             Double newScore = fun.apply(item, score);
 
             // Set the score
-            this.scores.put(item, newScore);
+            this.objectScores.put(item, newScore);
         });
+
+        // Reset the ordered list cache
+        this.orderedObjects = null;
     }
 
     public void remove(Function<T, Boolean> fun) {
@@ -57,7 +78,10 @@ public class Filter<T> {
     }
 
     public void remove(T singleItem) {
-        this.scores.put(singleItem, null);
+        this.objectScores.put(singleItem, null);
+
+        // Reset the ordered list cache
+        this.orderedObjects = null;
     }
 
     public void keepOnly(Function<T, Boolean> fun) {
@@ -73,14 +97,17 @@ public class Filter<T> {
     public void reset() {
 
         // Set all scores to 0
-        this.scores.replaceAll((T item, Double score) -> 0.0);
+        this.objectScores.replaceAll((T item, Double score) -> 0.0);
+
+        // Reset the ordered list cache
+        this.orderedObjects = null;
     }
 
     public int getCount() {
 
         int count = 0;
 
-        for (Double score : this.scores.values()) {
+        for (Double score : this.objectScores.values()) {
             if (score != null) {
                 count++;
             }
@@ -98,15 +125,31 @@ public class Filter<T> {
     }
 
     public void forEach(BiConsumer<T, Double> consumer) {
-        this.scores.forEach((item, score) -> {
+        this.objectScores.forEach((T item, Double score) -> {
             if (item != null) {
                 consumer.accept(item, score);
             }
         });
     }
 
+    public void forEachRemoved(Consumer<T> consumer) {
+        this.objectScores.forEach((T item, Double score) -> {
+            if (item == null) {
+                consumer.accept(item);
+            }
+        });
+    }
+
     public @Nullable T getBest() {
-        return null;
+        // Get the ordered list
+        List<T> ordered = this.getOrdered();
+
+        // Ensure there is data
+        if (ordered.size() > 0) {
+            return ordered.get(0);
+        } else {
+            return null;
+        }
     }
 
     public void withBest(Consumer<T> consumer) {
@@ -121,7 +164,15 @@ public class Filter<T> {
     }
 
     public @Nullable T getWorst() {
-        return null;
+        // Get the ordered list
+        List<T> ordered = this.getOrdered();
+
+        // Ensure there is data
+        if (ordered.size() > 0) {
+            return ordered.get(ordered.size() - 1);
+        } else {
+            return null;
+        }
     }
 
     public void withWorst(Consumer<T> consumer) {
@@ -135,8 +186,13 @@ public class Filter<T> {
         }
     }
 
-    public T[] getAboveThreshold(double threshold) {
-        return null;
+    public ImmutableList<T> getAboveThreshold(double threshold) {
+
+        List<T> output = new ArrayList<T>();
+
+        forEachAboveThreshold(threshold, (T object) -> output.add(object));
+
+        return ImmutableList.copyOf(output);
     }
 
     public void forEachAboveThreshold(double threshold, Consumer<T> consumer) {
@@ -151,8 +207,14 @@ public class Filter<T> {
         });
     }
 
-    public T[] getBelowThreshold(double threshold) {
-        return null;
+    public ImmutableList<T> getBelowThreshold(double threshold) {
+
+        List<T> output = new ArrayList<T>();
+
+        forEachBelowThreshold(threshold, (T object) -> output.add(object));
+
+        return ImmutableList.copyOf(output);
+
     }
 
     public void forEachBelowThreshold(double threshold, Consumer<T> consumer) {
@@ -167,12 +229,50 @@ public class Filter<T> {
         });
     }
 
-    public T[] getRemaining() {
-        return null;
+    public ImmutableList<T> getOrdered() {
+
+        // If there is not already a cached ordered list, create it
+        if (this.orderedObjects == null) {
+
+            // Get entry list
+            Set<Entry<T, Double>> entrySet = this.objectScores.entrySet();
+            List<Entry<T, Double>> entryList = new ArrayList<Entry<T, Double>>(entrySet);
+
+            // Sort the list
+            Collections.sort(entryList, new Comparator<Entry<T, Double>>() {
+                @Override
+                public int compare(Entry<T, Double> obj1, Entry<T, Double> obj2) {
+                    return obj2.getValue().compareTo(obj2.getValue());
+                }
+            });
+
+            // Allocate a new array
+            this.orderedObjects = new LinkedList<T>();
+
+            // Add all remaining objects in order
+            for (Entry<T, Double> entry : entryList) {
+                this.orderedObjects.add(entry.getKey());
+            }
+
+        }
+
+        return ImmutableList.copyOf(this.orderedObjects);
     }
 
-    public T[] getRemoved() {
-        return null;
+    public ImmutableList<T> getRemaining() {
+        List<T> output = new ArrayList<T>();
+
+        forEach((T object) -> output.add(object));
+
+        return ImmutableList.copyOf(output);
+    }
+
+    public ImmutableList<T> getRemoved() {
+        List<T> output = new ArrayList<T>();
+
+        forEachRemoved((T object) -> output.add(object));
+
+        return ImmutableList.copyOf(output);
     }
 
 }
